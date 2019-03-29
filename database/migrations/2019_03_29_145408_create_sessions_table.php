3d8110a03ef0d79b3735ecf00810a0c97758077e
lawfyd <dimonst93333@gmail.com>
2019-03-29 14:57:29 +0000

    add new sessin table

--- database/migrations/2019_03_29_145408_create_sessions_table.php
+++ database/migrations/2019_03_29_145408_create_sessions_table.php
@@ -14,9 +14,13 @@ class CreateSessionsTable extends Migration
     public function up()
     {
         Schema::create('sessions', function (Blueprint $table) {
-            $table->increments('id');
-            $table->ipAddress('ip_address');
-            $table->string('browser_name');
+            $table->string('id')->unique();
+            $table->unsignedInteger('user_id')->nullable();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->text('payload');
+            $table->integer('last_activity');
+            $table->string('browser_name')->nullable();
         });
     }
 
